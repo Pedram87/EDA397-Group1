6eb833028b622c3aeb38ddfe3e4c80d21f1b550f
mkuehl <maxmimilianlkuehl@web.de>
2016-04-19 15:23:24 +0200

    Added LoginActivity

--- app/src/main/java/com/group1/eda_397_group1/MainActivity.java
+++ app/src/main/java/com/group1/eda_397_group1/MainActivity.java
@@ -6,10 +6,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //redirecting to login/register
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
