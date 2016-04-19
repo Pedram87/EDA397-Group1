569978b6ac9d225ec3eec38a64be60cbc5d5d68b
bandam <vision.ami4@gmail.com>
2016-04-19 16:10:20 +0200

    Testing Number Picker

--- app/src/main/java/com/group1/eda_397_group1/CreateTaskActivity.java
+++ app/src/main/java/com/group1/eda_397_group1/CreateTaskActivity.java
@@ -2,6 +2,12 @@ package com.group1.eda_397_group1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class CreateTaskActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class CreateTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+
+
+        EditText taskName = (EditText) findViewById(R.id.taskName);
+        TextView taskOwnerLabel = (TextView) findViewById(R.id.taskOwnerLabel);
+        NumberPicker spinnerDuration = (NumberPicker) findViewById(R.id.durationPicker);
+        Button createTaskButton = (Button) findViewById(R.id.createTaskButton);
+
+
+
     }
 }
