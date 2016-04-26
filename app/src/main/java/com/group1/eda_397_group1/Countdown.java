497c943ad60ff7a4b4c2211397d4abc4cb2739ce
kathy5866 <sereti@student.chalmers.se>
2016-04-26 12:10:55 +0200

    added to manifest - still not working

--- app/src/main/java/com/group1/eda_397_group1/Countdown.java
+++ app/src/main/java/com/group1/eda_397_group1/Countdown.java
@@ -1,9 +1,12 @@
 package com.group1.eda_397_group1;
 
 import android.media.ToneGenerator;
+import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -13,10 +16,19 @@ public class Countdown extends AppCompatActivity {
     private TextView txt;
     CountDownTimer cdTimer;
 
-    Countdown(long length){
-        timeLength = length;
-        txt = (TextView) findViewById(R.id.timeView);
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acticity_countdown);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button start = (Button) findViewById(R.id.startCount);
+        Button stop = (Button) findViewById(R.id.stopCount);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
 
     public void setTime(View view, long tm){
         String toSend;
