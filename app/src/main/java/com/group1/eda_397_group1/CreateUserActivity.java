ea380ee7f0c1f5e016247b2894fa6e504a3e754b
Pedram <pedramshirin@hotmail.com>
2016-04-28 10:15:50 +0100

    Not much
    Not many changes, started on design.

--- app/src/main/java/com/group1/eda_397_group1/CreateUserActivity.java
+++ app/src/main/java/com/group1/eda_397_group1/CreateUserActivity.java
@@ -1,5 +1,6 @@
 package com.group1.eda_397_group1;
 
+import android.support.annotation.CallSuper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class CreateUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+
+
     }
 }
