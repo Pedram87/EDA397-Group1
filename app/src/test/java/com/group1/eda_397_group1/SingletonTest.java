a1ab4a678eacc29f4daa425f4a98ad4ce3f9e7ad
mkuehl <maxmimilianlkuehl@web.de>
2016-05-10 14:33:36 +0200

    created user singleton as class variable

--- app/src/test/java/com/group1/eda_397_group1/SingletonTest.java
+++ app/src/test/java/com/group1/eda_397_group1/SingletonTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertNotNull;
  */
 public class SingletonTest {
 
+    UserSingleton superuser;
     @Test
     public void singleton_creation_worksCorrect() throws NullPointerException, Exception {
         UserSingleton user = UserSingleton.getInstance();
@@ -22,5 +23,17 @@ public class SingletonTest {
         UserSingleton user = UserSingleton.getInstance();
         user.setEmail("testuser@testdomain.com");
         assertEquals("testuser@testdomain.com", user.getEmail());
+    }
+
+    private void setSuperuser() {
+        superuser = UserSingleton.getInstance();
+        superuser.setEmail("1@1.com");
+    }
+
+    @Test
+    public void singleton_staysCorrect() throws Exception {
+        setSuperuser();
+        superuser = UserSingleton.getInstance();
+        assertEquals("1@1.com", superuser.getEmail());
     }
 }
